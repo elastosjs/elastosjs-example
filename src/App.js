0ad55f6266b4f4d67c754afc985a5a25e2f4d4bf
Clarence Liu <clarence.hl.liu@gmail.com>
2020-05-01 04:29:17 -0700

    updated for ela-js 0.0.4

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { fromConnection, ephemeral } from '@openzeppelin/network'
-import { ELA_JS } from 'ela-js'
+import { elajs } from 'ela-js'
 
 const TESTNET_RPC = 'http://localhost:8545'
-const ELAJS_DATABASE_CONTRACT_ADDRESS = '0x98F1328BB668C7A4103c9140CfFC753caEc66ee4'
+const ELAJS_DATABASE_CONTRACT_ADDRESS = '0x6E31a56B63661Fd43FFB8F6c1023D35c64Ea3FF7'
+const ELA_RELAY_HUB_ADDR = '0xD216153c06E857cD7f72665E0aF1d7D82172F494'
 
 function App() {
 
   const [web3, setWeb3] = useState()
-  const [elajs, setElajs] = useState()
+  const [elajsDb, setElajsDb] = useState()
   const [todos, setTodos] = useState([])
   const [gsnBalance, setGsnBalance] = useState(0)
 
@@ -29,36 +30,37 @@ function App() {
         pollInterval: 5000
       })
 
-      const newElajs = new ELA_JS({
+      const elajsDb = new elajs.database({
         ephemeralWeb3: ephemeralWeb3,
 
         // make sure this is correct
-        contractAddress: ELAJS_DATABASE_CONTRACT_ADDRESS
+        databaseContractAddr: ELAJS_DATABASE_CONTRACT_ADDRESS,
+        relayHubAddr: ELA_RELAY_HUB_ADDR
       })
 
       setWeb3(ephemeralWeb3)
-      setElajs(newElajs)
+      setElajsDb(elajsDb)
     })()
   }, [ephemeral])
 
   /*
   ***************************************************************************
-  * This calls getGSNBalance() on elajs to get the balance
+  * This calls getGSNBalance() on elajsDb to get the balance
   ***************************************************************************
    */
   useEffect(() => {
     (async () => {
-      if (!elajs){
+      if (!elajsDb){
         return
       }
-      const gsnBalance = await elajs.getGSNBalance()
+      const gsnBalance = await elajsDb.getGSNBalance()
       setGsnBalance(gsnBalance)
     })()
-  }, [elajs, todos])
+  }, [elajsDb, todos])
 
   /*
   ***************************************************************************
-  * This function calls elajs's insertVal method
+  * This function calls elajs's insertRow method
   ***************************************************************************
    */
   const todoInput = useRef()
@@ -67,45 +69,42 @@ function App() {
 
     const todoText = todoInput.current.value
 
+    // this will hold the new item
     const newTodo = {
       id: null,
-      text: todoText,
-      transactionHash: null
+      task: todoText
     }
 
-    const todoBytes32 = web3.lib.utils.stringToHex(todoText)
+    const cols = ['task', 'done']
+    const vals = [todoText, false]
 
-    const insertPromise = elajs.insertVal('todos', 'todo', todoBytes32)
+    const rowId = await elajsDb.insertRow('todo', cols, vals)
 
-    insertPromise.on('transactionHash', (hash) => {
-      newTodo.transactionHash = hash
-    })
-
-    await insertPromise
+    newTodo.id = rowId
 
     todos.push(newTodo)
 
+    // slice to ensure we have a new array
     setTodos(todos.slice())
 
     todoInput.current.value = ''
 
-  }, [todoInput, web3, elajs, todos, setTodos])
+  }, [todoInput, web3, elajsDb, todos, setTodos])
 
   /*
   ***************************************************************************
   * This useEffect queries the data on load
-  * TODO: create a better elajs function for this
   ***************************************************************************
    */
   useEffect(() => {
     (async () => {
 
-      if (!elajs){
+      if (!elajsDb){
         return
       }
 
       // first get the ids
-      const todoIds = await elajs.getTableIds('todos')
+      const todoIds = await elajsDb.getTableIds('todo')
 
       const todos = []
 
@@ -113,18 +112,23 @@ function App() {
 
         const id = todoIds[i]
 
-        const result = await elajs._getVal('todos', id, 'todo')
+        const result = await elajsDb.getRow('todo', id)
 
-        todos.push({
-          id,
-          text: web3.lib.utils.hexToString(result)
+        const todo = {
+          id
+        }
+
+        result.forEach((colData) => {
+          todo[colData.name] = colData.value
         })
+
+        todos.push(todo)
       }
 
       setTodos(todos)
     })()
 
-  }, [web3, elajs, setTodos])
+  }, [web3, elajsDb, setTodos])
 
   return (
     <div className="App">
@@ -135,7 +139,7 @@ function App() {
               Contract Address: {ELAJS_DATABASE_CONTRACT_ADDRESS}
             </Col>
           </Row>
-          Your GSN Balance is {web3.lib.utils.fromWei(gsnBalance.toString())} ELASC
+          Your GSN Balance is {web3.lib.utils.fromWei(gsnBalance.toString())} ELAETHSC
 
           <Card>
             <CardHeader>
@@ -146,10 +150,18 @@ function App() {
             </CardHeader>
           </Card>
 
-          <ListGroup>
+          <ListGroup className="text-left">
             {todos.map((todo) => {
-              return <ListGroupItem style={{fontSize: '18px', color: 'black'}}>
-                {todo.text}
+              return <ListGroupItem key={todo.id} style={{fontSize: '18px', color: 'black'}}>
+                <Row>
+                  <Col>
+                    {todo.task}
+                  </Col>
+                  <Col>
+
+                  </Col>
+                </Row>
+
               </ListGroupItem>
             })}
           </ListGroup>
